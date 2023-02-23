3be091a2006662a17beef383e624ccd1f8ebbd19
LARRY MOSQUERA <larrysalsa@icloud.com>
2023-02-22 20:27:50 -0500

    Agregar archivos de proyecto.

---
+++ WebServiceSOAPEntityFramework/Servicios/WebService1.asmx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using WebServiceSOAPEntityFramework.Modelo;
+
+namespace WebServiceSOAPEntityFramework.Servicios
+{
+    /// <summary>
+    /// Descripción breve de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+        empleadossEntities emp= new empleadossEntities(); 
+
+        [WebMethod]
+        public List<usuarios> GetAllUser(empleadossEntities empleadossEntities)
+        {
+            return empleadossEntities.usuarios.ToList();
+        }
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "SOY UN PROGRAMADOR EXCELENTE";
+
+        }
+
+
+    }
+}
